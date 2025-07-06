5369c50d9f193d3e7ef7d42b0ae0315314a91ca6
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2025-07-06 16:04:41 -0600

    fix sql

--- server/sv_init.lua
+++ server/sv_init.lua
@@ -3,15 +3,16 @@ local init = {
     query = [[
         CREATE TABLE IF NOT EXISTS `xt_printers` (
             `id` INT(11) NOT NULL AUTO_INCREMENT,
-            `model` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-            `coords` LONGTEXT NOT NULL DEFAULT json_object() COLLATE 'utf8_general_ci',
-            `storage` LONGTEXT NOT NULL DEFAULT json_object() COLLATE 'utf8_general_ci',
-            `group` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+            `model` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+            `coords` LONGTEXT NOT NULL DEFAULT json_object() COLLATE 'utf8mb3_general_ci',
+            `storage` LONGTEXT NOT NULL DEFAULT json_object() COLLATE 'utf8mb3_general_ci',
+            `group` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
             `public` INT(11) NULL DEFAULT '0',
             PRIMARY KEY (`id`) USING BTREE
         )
-        COLLATE='utf8_general_ci'
+        COLLATE='utf8mb3_general_ci'
         ENGINE=InnoDB
+        AUTO_INCREMENT=1
         ;
     ]]
 }
